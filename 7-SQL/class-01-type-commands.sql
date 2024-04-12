00f1bb068681b0237329c51ed56c9c2f1d54b524
Gilsara Moreno <gilsaramore004@gmail.com>
2024-04-12 16:16:54 -0300

    add class 01 type commands type

--- 7-SQL/class-01-type-commands.sql
+++ 7-SQL/class-01-type-commands.sql
@@ -9,4 +9,35 @@ CREATE TABLE  IF NOT EXISTS students (
 
 -- DML
 INSERT INTO students (name, cpf, age) 
-VALUES ('Gohan', '094.454.593-03', 25);
+VALUES ('GOKU', '094.454.593-13', '20'
+);
+
+-- DQL
+SELECT name, age FROM students;
+
+SELECT * FROM students 
+WHERE id >= 2 LIMIT 3;
+
+SELECT * FROM students
+WHERE age >=18 and age <=67;
+
+--DML
+UPDATE students 
+SET age = 36
+WHERE id = 1;
+
+DELETE FROM students 
+WHERE id =3;
+
+--TCL
+BEGIN TRANSACTION;
+ROLLBACK;
+COMMIT;
+
+--DDL
+ALTER TABLE students
+ADD COLUMN anime TEXT;
+
+UPDATE students
+SET anime = 'drangonball'
+WHERE id = 8;
